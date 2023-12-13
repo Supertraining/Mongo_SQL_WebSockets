3975fc8b40536e6b3b0734ef30ddc8655359a624
MatíasJMaranga <marangamatias@gmail.com>
2023-12-13 11:40:59 -0300

    pm2Added

---
+++ ecosystem.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: "my-app",
+    script: "./server.js",
+    exec_mode: "cluster",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
